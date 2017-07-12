38a8d70c73cda7df31c2777f23db9006e6653f52
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-12 13:17:12 +0200

    Add radius calculation to layout engine

--- BasicClue/SaveDate/CircularControlLayoutEngine.swift
+++ BasicClue/SaveDate/CircularControlLayoutEngine.swift
@@ -1,1 +1,15 @@
 import Foundation
+
+struct CircularControlLayoutEngine {
+    private let viewSideLength: Double
+    private let outlineWidth: Double
+
+    var radius: Double {
+        return (viewSideLength / 2) - (2 * outlineWidth)
+    }
+
+    init(sideLength: Double, lineWidth: Double) {
+        viewSideLength = sideLength
+        outlineWidth = lineWidth
+    }
+}
--- BasicClueTests/SaveDate/CircularControlLayoutEngineTests.swift
+++ BasicClueTests/SaveDate/CircularControlLayoutEngineTests.swift
@@ -1,5 +1,17 @@
 import XCTest
+@testable import BasicClue
 
 class CircularControlLayoutEngineTests: XCTestCase {
     
+    // MARK: radius tests
+
+    func test_radius_sideLength10lineWidth1_returns3() {
+        let subject = CircularControlLayoutEngine(sideLength: 10, lineWidth: 1)
+        XCTAssertEqual(3, subject.radius)
+    }
+
+    func test_radius_sideLength40lineWidth3_returns14() {
+        let subject = CircularControlLayoutEngine(sideLength: 40, lineWidth: 3)
+        XCTAssertEqual(14, subject.radius)
+    }
 }
