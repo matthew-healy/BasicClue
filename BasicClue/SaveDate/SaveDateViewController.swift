4835d1b296e33049197625fb2e9e5aa478c102c1
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-12 17:52:30 +0200

    Add circular button centered in control

--- BasicClue/SaveDate/SaveDateViewController.swift
+++ BasicClue/SaveDate/SaveDateViewController.swift
@@ -2,4 +2,15 @@ import UIKit
 
 class SaveDateViewController: UIViewController {
 
+    @IBOutlet weak var saveDateButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        saveDateButton.layer.masksToBounds = true
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        saveDateButton.layer.cornerRadius = saveDateButton.frame.width / 2
+    }
 }
