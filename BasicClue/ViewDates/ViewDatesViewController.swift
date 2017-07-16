7946f1dd08a6943f58c98e24b0d8181452aac116
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-16 11:57:58 +0200

    Add ViewDatesViewController

---
+++ BasicClue/ViewDates/ViewDatesViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewDatesViewController: UIViewController {
+
+    @IBOutlet weak var datesTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
